e8348cbfbd38efb93715e81ad16d82a6c15b8096
Edwin Antonio <antonedw@uw.edu>
2021-04-20 19:57:44 -0700

    More restructuring to further genralize code

--- PlanPrepareProcess/Plan/CreateSamples_renewed.py
+++ PlanPrepareProcess/Plan/CreateSamples_renewed.py
@@ -8,6 +8,8 @@ from pytz import timezone
 import csv
 
 
+##### Set up the experiment plan dictionary to be referenced for useful information throughout a design of experiments #####
+
 def get_experiment_plan(filepath, chemical_database_path):
     """
     Parse a .csv file to create a dictionary of instructions.
@@ -37,21 +39,25 @@ def component_order_dictionary(plan):
     return component_order_dict
 
 
-##### Create the concentrations dataframe from components (or if simple case the stocks themselves) #####
+##### Create the concentrations dataframe from components (Or in the simple case of not being concerned with final component concentration, this step would be calculating the percentages of each stock) #####
 
-def determine_concentration_path(concentration_variable, variable_type):
-    # could do the thing were just blatantly state which thing, csv, excel, linspace, list... maybe use of kwargs will simplify this.
-    if 'variable_type' == 'csv':
-        return concentration_variable
-    elif 'variable_type' == 'excel':
-        return concentration_variable
-    elif variable_type == 'linspace':
-        pass
-    elif variable_type == 'sublists':
-        pass
+def concentration_from_csv(csv_path):
+    """Given a path to a csv will translate to the information to a dataframe in the default nature of pandas.
+    Data is formatted based on column and spacing, hence in a csv the first row will headers seperated by commas and the next row will 
+    be respective header values seperated by commas."""
+    concentration_df = pd.read_csv(csv_path)
+    return concentration_df 
+
+def concentration_from_excel(excel_path):
+    """Given a path to an excel file (xlsx) will translate information to a dataframe in the default nature of pandas.
+    Data is formatted based on the same row and column order within the excel sheet.
+    Ensure the headers match the names of components of a plan as information
+    """
+    concentration_df = pd.read_excel(excel_path)
+    return concentration_df
 
 def concentration_from_linspace(plan, unity_filter = False):
-    """ Uses linspaces to create a mesh of component concentrations
+    """ Uses linspaces to create a mesh of component concentrations. The linspaces
     """
     component_linspaces = plan['Component Concentration Linspaces [min, max, n]']
     component_spacing_type = 'linear'
@@ -77,14 +83,6 @@ def concentration_from_linspace(plan, unity_filter = False):
     if unity_filter == True:
         unity_filter_df(concentration_df, component_names, component_units)
 
-    return concentration_df
-
-def concentration_from_csv(csv_path):
-    concentration_df = pd.read_csv(csv_path)
-    return concentration_df 
-
-def concentration_from_excel(excel_path):
-    concentration_df = pd.read_excel(excel_path)
     return concentration_df
 
 def concentration_from_list_samplewise(plan, conc_sublists):
@@ -125,10 +123,183 @@ def unity_filter_df(concentration_df, component_names, component_units):
 
 mass_dictionary = {'g':1} # should build a dictionary of units people can add to such that not restricted to hardcoded ones
 
+
+
+def identify_unit(string):
+    """Based on a provided string will identify if a unit within a list verified working units"""
+    supported_units = ['wtf','volf','molf','mgpermL','molarity', 'g', 'mL', 'g/mL', 'g/mol']
+    for unit in supported_units:
+        if unit in string:            
+            return unit
+    raise AssertionError('Unit in ' + string + ' not currently supported, the following units are supported: ', supported_units)
+        
+def identify_component_name(string):
+    """Will pull the first word from the string and return it to be used as the name to look up in a chemical database.
+    Can also make this is a checkpoint to ensure the component is in the chemical database to begin with. This allows you 
+    to contain all information within a column (component identity, name)"""
+    component = string.split(' ', 1)[0]
+    return component
+
+
+def determine_component_mass(total_sample_amount, total_sample_amount_unit, component_values, component_unit, component_info):
+    """Determines the mass of a component (series or single value) based on the total sample unit and the component unit. 
+    Hence there are a finate number of unit combinations which will work, the easiest way to think about this look the numerator 
+    and denominator of the component unit and determine how to remove the denominator."""
+    
+    if total_sample_amount_unit == 'g' and component_unit == 'wtf':
+        component_masses = total_sample_amount*component_values
+
+    elif total_sample_amount_unit == 'mL' and component_unit == 'mgpermL':
+        component_masses = total_sample_amount*component_values/1000 # for now default mass = g, volume = mL
+
+    elif total_sample_amount_unit == 'mL' and component_unit == 'molarity':
+        molecular_weight = component_info['Molecular Weight (g/mol)']
+        component_masses = total_sample_amount*component_values*molecular_weight/1000
+    
+    else: 
+        raise AssertionError(total_sample_amount_unit, 'and', component_unit, 'units are not supported to calculate for mass')
+    print('You calculated for component masses given the provided units')
+    return component_masses
+
+def determine_component_volumes(total_sample_amount, total_sample_amount_unit, component_values, component_unit, component_info):
+    """Determines the volume of a component (series or single value) based on the total sample unit and the component unit. 
+    Hence there are a finate number of unit combinations which will work, the easiest way to think about this look the numerator 
+    and denominator of the component unit and determine how to remove the denominator."""
+    
+    if total_sample_amount_unit == 'mL' and component_unit == 'volf':
+        component_volume = total_sample_amount*component_values
+        print('You calculated for component volumes given the provided units')
+        return component_volume
+
+
+def determine_component_amounts(concentration_df, plan):
+    """Based on plan information (Component Names and total sample unit) will determine the amount of each component (mass or volume) 
+    required for each sample. Currently only supports mL and g as default units. 
+    It is recommended you keep plan lists in order i.e component[2] refers to the third column of components."""
+    
+    component_info_dict = plan['Chemical Database']
+    component_names = plan['Component Shorthand Names']
+    total_sample_amount_unit = plan['Sample Unit']
+    total_sample_amount = plan['Sample Amount']
+    
+    for column_name, component_name in zip(concentration_df, component_names): # change to just iterate through column name
+        if component_name in column_name: # could just use name to call specfic column versus iterating?
+            component_values = concentration_df[column_name]
+            component_info = component_info_dict[component_name]
+            component_unit = identify_unit(column_name)
+            if component_unit == 'wtf' or 'mgpermL' or 'molarity': # these are the unit that lead to mass outcomes
+                component_masses = determine_component_mass(total_sample_amount, total_sample_amount_unit, component_values, component_unit, component_info)  
+                concentration_df[component_name + ' amount mass ' + total_sample_amount_unit]  = component_masses
+            if component_unit == 'volf': # these are the unit that lead to volume outcomes
+                component_volume = determine_component_volumes(total_sample_amount, total_sample_amount_unit, component_values, component_unit, component_info)  
+                concentration_df[component_name + ' amount volume ' + total_sample_amount_unit]  = component_masses
+    return concentration_df
+
+
+def calculate_component_amount_missing(component_amounts_df, plan):
+    chemical_database = plan['Chemical Database']
+    
+    for amount_col in component_amounts_df:
+        component_amount = component_amounts_df[amount_col]
+        component = identify_component_name(amount_col)
+        component_unit = identify_unit(amount_col)
+        component_info = chemical_database[component]
+        component_density = component_info['Density (g/mL)']
+        
+        if 'mass' in amount_col: # could make a small function to deteremine amount type
+            component_volumes = component_amount/component_density
+            component_amounts_df[amount_col.replace('mass '+ component_unit, 'volume mL')] = component_volumes
+            print('You calculated component volumes from a component mass using ' + component  + ' density of ' + str(component_density) + ' g/mL' )
+        elif 'volume' in amount_col:
+            component_masses = component_amount*component_density
+            component_amounts_df[amount_col.replace('volume '+ component_unit, 'mass g')] = component_masses
+            print('You calculated component masses from a component volumes using ' + component  + ' density of ' + str(component_density) + ' g/mL' )
+#         else: # this wont work as it will call out other columns
+#             raise AssertionError('Component amounts not present. Amount headers need to be formatted as ComponentName_amount_volumeormass_unit, example: dppc amount volume mL')
+
+    return component_amounts_df
+# be careful not to run this twice as it will overwrite some stuff
+
+def nan_amounts(amounts_df):
+    amounts_df_zeroed = amounts_df.fillna(0)
+    return amounts_df_zeroed
+
+
+##################### In progress ##############################
+
+def concentration_from_linspace_all_info(plan, unity_filter = False): # if you go this route you can do whole dataframe operation you just need to verify all component units of the same type
+    """ Uses linspaces to create a mesh of component concentrations
+    """
+    component_linspaces = plan['Component Concentration Linspaces [min, max, n]']
+    component_spacing_type = 'linear'
+
+    conc_range_list = [] 
+    for conc_linspace in component_linspaces:
+        if component_spacing_type == "linear": 
+            conc_range_list.append(np.linspace(*conc_linspace))
+    conc_grid = np.meshgrid(*conc_range_list)
+    
+    total_sample_amount = plan['Sample Amount']
+    total_sample_amount_unit = plan['Sample Unit']
+    component_names = plan['Component Shorthand Names']
+    component_units = plan['Component Concentration Units']
+    
+    data = []
+    columns = []
+    for component_index in range(len(conc_grid)): 
+        n = len(conc_grid[component_index].ravel())
+        
+        component_name_entry = [component_names[component_index]]*n
+        columns.append('Component ' + str(component_index) + ' Name')
+        data.append(component_name_entry)
+        
+        component_unit_entry = [component_units[component_index]]*n
+        columns.append('Component ' + str(component_index) + ' Concentration Unit')
+        data.append(component_unit_entry)
+        
+        component_concentration_column = 'Component ' + str(component_index) + ' Concentration Value'
+        component_concetration_values = conc_grid[component_index].ravel()
+        columns.append(component_concentration_column)
+        data.append(component_concetration_values)
+        
+    component_conc_df = pd.DataFrame(data, columns).T # will terminate here if not needed unity
+        
+    if unity_filter == True: # generalize this  and make into callable function
+        final_component_index = component_index + 1 
+        
+        component_name_entry = [component_names[final_component_index]]*n
+        columns.append('Component ' + str(final_component_index) + ' Name')
+        data.append(component_name_entry)
+        
+        component_unit_entry = [component_units[final_component_index]]*n
+        columns.append('Component ' + str(final_component_index) + ' Concentration Unit')
+        data.append(component_unit_entry)
+        
+        concentration_values_isolated = component_conc_df[[col for col in component_conc_df.columns if 'Concentration Value' in col]]
+        completing_concentration_values = (1 - concentration_values_isolated.sum(axis=1)).tolist()
+        data.append(completing_concentration_values)
+        columns.append('Component ' + str(final_component_index) + ' Concentration Value')
+
+    component_conc_df = pd.DataFrame(data, columns).T
+    
+    component_conc_df.insert(loc=0, column = 'Total Sample Amount Unit', value = [total_sample_amount_unit]*n)
+    component_conc_df.insert(loc=0, column = 'Total Sample Amount', value = [total_sample_amount]*n) # this needs to be added at the same amount
+
+    return component_conc_df
+
+def determine_concentration_path(concentration_variable, variable_type):
+    """ Determines the appropiate path to handle and create concentration design space... Still in progress requires kwargs
+    """
+    if 'variable_type' == 'csv':
+        return cconcentration_variable
+    elif 'variable_type' == 'excel':
+        return concentration_variable
+    elif variable_type == 'linspace':
+        pass
+    elif variable_type == 'sublists':
+        pass
+
 def determine_unit_pathway(plan, concentration_df):
     components_concentration_units = plan['Component Concentration Units']
 
     pass
-
-
-
